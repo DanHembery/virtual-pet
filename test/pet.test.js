b7f10ddb8354fa9b5cd6b3a58ff286ca1c2a5bb4
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-12-13 17:09:59 +0000

    feed function defined and code refactored

--- test/pet.test.js
+++ test/pet.test.js
@@ -14,16 +14,54 @@ describe('constructor', () => {
   });
 
   describe('constructor', () => {
-    it('has a initial age of 0', () => {
+    it('initial age is 0, hunger is 0 and fitness is 10', () => {
       const pet = new Pet('Fido');
       expect(pet.age).toEqual(0);
+      expect(pet.hunger).toEqual(0);
+      expect(pet.fitness).toEqual(10);
     });
   });
 
   describe('growUp', () => {
-    it('increments the age by 1', () => {
+    it('increments age by 1, hunger by 5, and decreases fitness by 3', () => {
       const pet = new Pet('Fido');
       pet.growUp();
       expect(pet.age).toEqual(1);
+      expect(pet.hunger).toEqual(5);
+      expect(pet.fitness).toEqual(7);
+    });
+  });
+
+  describe('walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('fido');
+      pet.fitness = 4;
+      pet.walk();
+      expect(pet.fitness).toEqual(8);
+    });
+  });
+
+  describe('walk', () => {
+    it('increases fitness by 4 to a maximum of 10', () => {
+      const pet = new Pet('fido');
+      pet.fitness = 8;
+      pet.walk();
+      expect(pet.fitness).toEqual(10);
+    });
+  });
+  describe('feed', () => {
+    it('decreases hunger by 3', () => {
+      const pet = new Pet('fido');
+      pet.hunger = 5;
+      pet.feed();
+      expect(pet.hunger).toEqual(2);
+    });
+  });
+  describe('feed', () => {
+    it('decreases hunger by 3 to a maximum of 0', () => {
+      const pet = new Pet('fido');
+      pet.hunger = 2;
+      pet.feed();
+      expect(pet.hunger).toEqual(0);
     });
   });
